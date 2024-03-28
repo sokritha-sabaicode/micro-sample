49f337593e8cbd13846f9c91ce2d9de3771408f6
sokritha-it <sokritha.it@gmail.com>
2024-03-28 07:48:54 +0700

    fix: generate route with tsoa

--- auth/build/src/middlewares/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.errorHandler = void 0;
-const error_handler_1 = require("./error-handler");
-Object.defineProperty(exports, "errorHandler", { enumerable: true, get: function () { return error_handler_1.errorHandler; } });
-//# sourceMappingURL=index.js.map
